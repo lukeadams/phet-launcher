a0e06d59d1c22ba67593e788e19c46921e7f767e
Luke Adams <luke4450@gmail.com>
2016-01-28 16:08:57 -0600

    bundle root (for testing) and sims array

--- app/index.js
+++ app/index.js
@@ -1,3 +1,5 @@
 var jsyaml = require("./bower_components/js-yaml/dist/js-yaml.min.js")
 var fs = require('fs');
 var Path = require('path')
+var bundle_root = Path.resolve("../../phet-scraper/bundle/")
+var sims = jsyaml.load(fs.readFileSync(Path.join(bundle_root, "config.yml")))
