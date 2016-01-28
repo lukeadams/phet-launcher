d9965baa24e7dc7c663c861bac226d52834bb369
Luke Adams <luke4450@gmail.com>
2016-01-28 16:09:11 -0600

    Add all sims to dom

--- app/index.js
+++ app/index.js
@@ -3,3 +3,12 @@ var fs = require('fs');
 var Path = require('path')
 var bundle_root = Path.resolve("../../phet-scraper/bundle/")
 var sims = jsyaml.load(fs.readFileSync(Path.join(bundle_root, "config.yml")))
+	for (sim of sims) {
+		sim_root = Path.join(bundle_root, sim[':url_hash'])
+		var _dom = $(`
+			<div class='sim mui-panel' data-sim-urlhash='${sim[":url_hash"]}'>
+				<img src="${Path.join(sim_root, sim[":image_file_name"])}"></img>
+				<p class='name'>${sim[":name"]}</p>
+			</div>
+		`).appendTo($('#sim-list'))
+	}
