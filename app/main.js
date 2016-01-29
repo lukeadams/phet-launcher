d976b57a6bf3598c1091eb4686c973aa80fcec4e
Luke Adams <luke4450@gmail.com>
2016-01-29 14:12:33 -0600

    Disable devtools

--- app/main.js
+++ app/main.js
@@ -13,9 +13,6 @@ function createWindow () {
   // and load the index.html of the app.
   mainWindow.loadURL('file://' + __dirname + '/app.html');
 
-  // Open the DevTools.
-  mainWindow.webContents.openDevTools();
-
   // Emitted when the window is closed.
   mainWindow.on('closed', function() {
     // Dereference the window object, usually you would store windows
