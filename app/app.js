93c510e36cd4ac8028d9493a1e579c0d683851c7
Luke Adams <luke4450@gmail.com>
2016-01-31 00:11:25 -0600

    Flash is the only one that has plugins enabled

--- app/app.js
+++ app/app.js
@@ -1,9 +1,9 @@
-var fs = require('fs');
-var Path = require('path')
-var jsyaml = require(Path.join(__dirname, "bower_components/js-yaml/dist/js-yaml.min.js"))
+const fs = require('fs');
+const Path = require('path')
+const jsyaml = require(Path.join(__dirname, "bower_components/js-yaml/dist/js-yaml.min.js"))
 
-var BrowserWindow = require('electron').remote.BrowserWindow;
-var bundle_root = Path.resolve(__dirname, "bundle")
+const BrowserWindow = require('electron').remote.BrowserWindow;
+const bundle_root = Path.resolve(__dirname, "bundle")
 var sims = jsyaml.load(fs.readFileSync(Path.join(bundle_root, "config.yml")))
 
 var child_proc = require('child_process')
@@ -11,7 +11,7 @@ var child_proc = require('child_process')
 $(function(){
 	for (sim of sims) {
 		sim_root = Path.join(bundle_root, sim[':url_hash'])
-		var _dom = $(`
+		$(`
 			<div class='sim mui-panel' data-sim-urlhash='${sim[":url_hash"]}'>
 				<img src="${Path.join(sim_root, sim[":image_file_name"])}"></img>
 				<p class='name'>${sim[":name"]}</p>
@@ -19,6 +19,7 @@ $(function(){
 		`).appendTo($('#sim-list'))
 	}
 
+	//Search. 
 	$('#search').keyup(function(event){
 		_text = event.delegateTarget.value
 		$('.sim').each(function(num, sim){
@@ -35,7 +36,7 @@ $(function(){
 		})
 	})
 
-	//Better search
+	//Better search. Focus on any keypress
 	$(document).keydown(function(e){
 		$('#search').focus()
 	});
@@ -53,7 +54,7 @@ $(function(){
 			launchJava(sim_path);
 		}
 		else if (sim[':type'] == ':flash'){
-			launchHtml(sim_path);
+			launchFlash(sim_path);
 		} else if (sim[':type'] == ':html'){
 			launchHtml(sim_path)
 		}
@@ -61,22 +62,26 @@ $(function(){
 })
 
 function launchHtml(path){
-	var win = new BrowserWindow({ width: 800, height: 600, show: false, webPreferences:{nodeIntegration:false, plugins: true, webSecurity:false}});
+	var win = new BrowserWindow({ width: 800, height: 600, show: false, webPreferences:{nodeIntegration:false}});
 	win.on('closed', function() {
 	  win = null;
 	});
 	win.setMenu(null);
-	win.openDevTools()
 	win.loadURL('file://' + path);
 	win.show();
 }
 function launchFlash(path){
-	//('file://' + path)
-	alert("This is a flash sim; launch it in Chrome.")
+	var win = new BrowserWindow({ width: 800, height: 600, show: false, webPreferences:{nodeIntegration:false, plugins: true}}); //Test websec true and false, prefer true
+	win.on('closed', function() {
+	  win = null;
+	});
+	win.setMenu(null);
+	win.loadURL('file://' + path);
+	win.show();
 }
 function launchJava(path){///Users/lukeadams/Projects/phet-launcher/app/java/osx/Contents/Home/bin
 	const java_binary = Path.join(__dirname, 'java', 'windows', 'bin', 'java.exe')
-	child_proc.exec(`${java_binary} -jar "${path}"`)
+	child_proc.exec(`${java_binary} -jar "${path}"`) //Move to spawn. options.detached and child.unref()
 }
 
 
